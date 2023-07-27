5157b3ffd110688f2811ba0bae9d3b18be1e68fa
nazrilganteng17 <139833638+nazrilganteng17@users.noreply.github.com>
2023-07-27 10:43:42 +0700

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,17 @@
+<template>
+ <navbar></navbar>
+ <br>
+ <Carousel></Carousel>
+ <br>
+ <TheCard></TheCard>
+ <br>
+ <Footer-Dua></Footer-Dua>
+</template>
+<script setup>
+import navbar from './navbar.vue';
+import Carousel from './header.vue'
+import  TheCard  from './card.vue';
+import FooterDua from './FooterDua.vue'
+
+
+</script>
